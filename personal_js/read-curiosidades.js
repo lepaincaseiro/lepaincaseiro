c63a09e4a808e4df5be3c254246dc77ffdf78c75
Messias Nunesmaia <messias.nunesmaia@ucsal.edu.br>
2020-09-10 20:28:42 -0300

    Sistema de mini-curiosidades funcionando

--- personal_js/read-curiosidades.js
+++ personal_js/read-curiosidades.js
@@ -1,26 +1,30 @@
 const db = firebase.database();
 const refPosts = db.ref('posts');
+const storage = firebase.storage();
+const storageRef = storage.ref('posts');
+
 
 refPosts.orderByKey().on('value', snapshot => {
-
+    
     var posts = snapshot.val();
     var keys = Object.keys(posts);
-
+    
     var pegandoElements='';
-
+    
     for (let i = 0; i < keys.length; i++) {
         var k = keys[i];
-        // var title = posts[k].title;
+        var title = posts[k].title;
         var sinopse = posts[k].sinopse;
         // var description = posts[k].description;
-        var photo = posts[k].photo;
+        var image = posts[k].image_url;
+        console.log(image);
         var updated = posts[k].updated;
         // var date = posts[k].date;
         // var createdOrUpdated='Criado em : ';
         // if (updated) {
         //     createdOrUpdated= 'Atualizado em : ';
         // } 
-        pegandoElements += '<div class="hoverzoom mx-auto my-3 my-md-auto"><img src="'+photo+'" alt="Imagem para Hoverzoom" /><div class="retina"><p>'+sinopse+'</p><a href="#">Saiba mais</a></div></div>';
+        pegandoElements += '<div class="hoverzoom mx-auto my-3 my-md-auto"><img src="'+image+'" alt="Imagem para Hoverzoom" /><div class="retina"><p>'+sinopse+'</p><a href="#">Saiba mais</a></div></div>';
     }
     var testandoLer = document.getElementById('hoverzoons');
     testandoLer.innerHTML = pegandoElements;
--- personal_js/real-time-database.js
+++ personal_js/real-time-database.js
@@ -1,19 +1,24 @@
 const db = firebase.database();
+const storage = firebase.storage();
 const rootRef = db.ref('posts');
+const storageRef = storage.ref('posts_images');
 var auth = firebase.auth();
 
 var postsList = document.getElementById('postsList');
 var title = document.getElementById('title');
 var sinopse = document.getElementById('sinopse');
 var description = document.getElementById('description');
-var photo = document.getElementById('photo');
+var image = document.getElementById('image');
 var updateSinopse = document.getElementById('updateSinopse');
 var updateDescription = document.getElementById('updateDescription');
 const addButton = document.getElementById('addButton');
 const updateButton = document.getElementById('updateButton');
 const deleteButton = document.getElementById('deleteButton');
 
-console.log(title);
+var image_url = "";
+
+console.log(image_url);
+
 
 function completeDate() {
 
@@ -36,29 +41,60 @@ addButton.addEventListener('click', (teste) => {
 
     teste.preventDefault();
 
-    const autoId = rootRef.push().key
-    rootRef.child(autoId).set({
 
-        title: title.value,
-        sinopse: sinopse.value,
-        description: description.value,
-        photo: photo.value,
-        date: completeDate(),
-        updated: false
-    })
-        .then(() => {
-            window.alert('Curiosidade adicionada com sucesso !');
-        })
-        .catch(error => {
-            auth.onAuthStateChanged(firebaseUser => {
-                if (firebaseUser) {
-                    window.alert('Erro inesperado !');
-                } else {
-                    window.alert('Acesso não permitido !');
-                }
+    // Upload image on Storage
+    var uploadTask = storageRef.child(title.value).put(image.files[0]);
+
+    uploadTask.on('state_changed', function (snapshot) {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+                console.log('Upload is paused');
+                break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+                console.log('Upload is running');
+                break;
+        }
+    },
+        function (error) {
+            window.alert('Erro ao mandar imagem para nuvem !');
+            console.log(error);
+        }, function () {
+            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+
+                // Add a JSON file on Realtime Database
+                const autoId = rootRef.push().key
+                rootRef.child(autoId).set({
+
+                    title: title.value,
+                    sinopse: sinopse.value,
+                    description: description.value,
+                    image_url: downloadURL,
+                    date: completeDate(),
+                    updated: false
+                })
+                    .then(() => {
+                        window.alert('Curiosidade adicionada com sucesso !');
+                    })
+                    .catch(error => {
+                        auth.onAuthStateChanged(firebaseUser => {
+                            if (firebaseUser) {
+                                window.alert('Erro inesperado !');
+                            } else {
+                                window.alert('Acesso não permitido !');
+                            }
+                        });
+                        console.error(error);
+                    });
+
+                console.log('File available at', downloadURL);
             });
-            console.error(error);
         });
+
+
 
 });
 
