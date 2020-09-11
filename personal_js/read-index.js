8528e84a3df86e9fc40936dad61495b0a61324bf
Messias Nunesmaia <messias.nunesmaia@ucsal.edu.br>
2020-09-11 11:36:12 -0300

    Atualização em redirecionamento de posts

--- personal_js/read-index.js
+++ personal_js/read-index.js
@@ -13,7 +13,7 @@ refPosts.orderByKey().on('value', snapshot => {
         var k = keys[i];
         var sinopse = posts[k].sinopse;
         var image = posts[k].image_url;
-        pegandoElements += '<div class="hoverzoom mx-auto my-3 my-md-auto"><img src="'+image+'" alt="Imagem para Hoverzoom" /><div class="retina"><p>'+sinopse+'</p><a href="#">Saiba mais</a></div></div>';
+        pegandoElements += '<div class="hoverzoom mx-auto my-3 my-md-auto"><img src="'+image+'" alt="Imagem para Hoverzoom" /><div class="retina"><p>'+sinopse+'</p><a href="./index.html">Saiba mais</a></div></div>';
     }
     var testandoLer = document.getElementById('hoverzoons');
     testandoLer.innerHTML = pegandoElements;
